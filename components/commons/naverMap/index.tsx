02e9558ad2e4c395652b02231afbed887c234698
Calmer_com <ty.calmer96@gmail.com>
2024-07-09 17:52:41 +0900

    feat: 클러스터 정적 폴더에 추가

--- components/commons/naverMap/index.tsx
+++ components/commons/naverMap/index.tsx
@@ -4,6 +4,7 @@ import type { NaverMapProps } from "@/commons/types";
 declare global {
   interface Window {
     naver: any;
+    MarkerClustering: any;
   }
 }
 
@@ -17,25 +18,26 @@ export default function NaverMap({
       script.type = "text/javascript";
       script.src = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${ncpClientId}`;
       script.async = true;
-      script.onload = initMap;
+      script.onload = loadMarkerClustering;
       document.head.appendChild(script);
     };
+
     const loadMarkerClustering = (): void => {
       const script = document.createElement("script");
       script.type = "text/javascript";
-      script.src = `./markerClustering.js`;
+      script.src = "/commons/libraries/markerClustering.js";
       script.async = true;
       script.onload = initMap;
       document.head.appendChild(script);
     };
-
-    // const htmlMarker1 = {
-    //   content:
-    //     '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(/example/images/cluster-marker-1.png);background-size:contain;"></div>',
-    //   size: N.Size(40, 40),
-    //   anchor: N.Point(20, 20),
-    // };
     const initMap = async (): Promise<void> => {
+      if (!window.naver || !window.MarkerClustering) {
+        console.error(
+          "네이버 맵 또는 마커 클러스터링 라이브러리가 로드되지 않았습니다."
+        );
+        return;
+      }
+
       const mapOptions = {
         center: new window.naver.maps.LatLng(37.3595704, 127.105399),
         zoom: 10,
@@ -71,7 +73,39 @@ export default function NaverMap({
         })
         .filter((marker) => marker !== null);
 
-      const markerClustering = new MarkerClustering({
+      // htmlMarker1 객체 정의
+      const htmlMarker1 = {
+        content:
+          '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(/example/images/cluster-marker-1.png);background-size:contain;"></div>',
+        size: new window.naver.maps.Size(40, 40),
+        anchor: new window.naver.maps.Point(20, 20),
+      };
+      const htmlMarker2 = {
+        content:
+          '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-2.png);background-size:contain;"></div>',
+        size: new window.naver.maps.Size(40, 40),
+        anchor: new window.naver.maps.Point(20, 20),
+      };
+      const htmlMarker3 = {
+        content:
+          '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://new window.naver.maps.github.io/maps.js.ncp/docs/img/cluster-marker-3.png);background-size:contain;"></div>',
+        size: new window.naver.maps.Size(40, 40),
+        anchor: new window.naver.maps.Point(20, 20),
+      };
+      const htmlMarker4 = {
+        content:
+          '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://new window.naver.maps.github.io/maps.js.ncp/docs/img/cluster-marker-4.png);background-size:contain;"></div>',
+        size: new window.naver.maps.Size(40, 40),
+        anchor: new window.naver.maps.Point(20, 20),
+      };
+      const htmlMarker5 = {
+        content:
+          '<div style="cursor:pointer;width:40px;height:40px;line-height:42px;font-size:10px;color:white;text-align:center;font-weight:bold;background:url(https://new window.naver.maps.github.io/maps.js.ncp/docs/img/cluster-marker-5.png);background-size:contain;"></div>',
+        size: new window.naver.maps.Size(40, 40),
+        anchor: new window.naver.maps.Point(20, 20),
+      };
+
+      const markerClustering = new window.MarkerClustering({
         minClusterSize: 2,
         maxZoom: 8,
         map,
@@ -86,7 +120,6 @@ export default function NaverMap({
             .querySelector("div:first-child").innerText = count;
         },
       });
-      console.log(markerClustering);
       // 보이는 곳만 마커 불러오기
       const updateMarkers = (map: any, markers: any): void => {
         const mapBounds = map.getBounds();
@@ -117,7 +150,6 @@ export default function NaverMap({
     };
 
     loadMapScript();
-    loadMarkerClustering();
   }, [geocodeResults, ncpClientId]);
   return <div id="map" style={{ width: "100%", height: "100%" }}></div>;
 }
