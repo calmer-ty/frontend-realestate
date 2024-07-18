96f49dde060c8d47070218a4c507813f2d0b7491
Calmer <ty.calmer96@gmail.com>
2024-07-19 08:18:53 +0900

    refactor: item data들 객체로 전환

--- src/commons/libraries/geocodeData.ts
+++ src/commons/libraries/geocodeData.ts
@@ -8,21 +8,22 @@ const geocodeCache = new NodeCache({ stdTTL: 7200 });
 export const geocodeData = async (): Promise<IGeocodeData[]> => {
   const apartmentResults: IApartmentLocationData[] = await apartmentData();
   const geocodePromises = apartmentResults.flatMap((result) => {
-    const items = result?.apartmentData?.response?.body?.items?.item ?? [];
-    return items.map(async (item) => {
-      const location = result.locatadd_nm;
-      const address = `${location} ${item.법정동.trim()} ${Number(item.법정동본번코드).toString()}`;
-      const apartmentName = item.아파트;
-      const amount = Number(item.거래금액.replace(/,/g, ""));
-      const area = item.전용면적;
-      const floor = item.층;
-      const dealYear = item.년;
-      const dealMonth = item.월;
-      const dealDay = item.일;
-      const cacheKey = `geocode_${address}`;
+    const apartmentDataItems = result?.apartmentData?.response?.body?.items?.item ?? [];
+    return apartmentDataItems.map(async (item) => {
+      const itemsData = {
+        address: `${result.locatadd_nm} ${item.법정동.trim()} ${Number(item.법정동본번코드).toString()}`,
+        apartmentName: item.아파트,
+        amount: Number(item.거래금액.replace(/,/g, "")),
+        area: item.전용면적,
+        floor: item.층,
+        dealYear: item.년,
+        dealMonth: item.월,
+        dealDay: item.일,
+      };
+      const cacheKey = `geocode_${itemsData.address}`;
 
       // 캐시에서 데이터를 가져오거나 새로 요청하여 캐시에 저장합니다
-      const cacheData = { address, apartmentName, amount, area, floor, dealYear, dealMonth, dealDay };
+      const cacheData = itemsData;
       const cachedData = geocodeCache.get<IGeocodeData>(cacheKey);
 
       if (cachedData !== undefined) {
@@ -31,7 +32,7 @@ export const geocodeData = async (): Promise<IGeocodeData[]> => {
       }
 
       try {
-        const apiUrl = `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${encodeURIComponent(address)}`;
+        const apiUrl = `https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query=${encodeURIComponent(itemsData.address)}`;
         const response = await axios.get<IGeocodeCoord>(apiUrl, {
           headers: {
             "X-NCP-APIGW-API-KEY-ID": process.env.NEXT_PUBLIC_NCP_CLIENT_ID,
@@ -51,11 +52,11 @@ export const geocodeData = async (): Promise<IGeocodeData[]> => {
           geocodeCache.set(cacheKey, geocodeResult);
           return geocodeResult;
         } else {
-          // console.log(`주소 ${address}에 대한 지오코딩 결과 없음`);
+          console.log(`주소 ${itemsData.address}에 대한 지오코딩 결과 없음`);
           return null;
         }
       } catch (error) {
-        console.error(`Error geocoding address ${address}:`, error);
+        console.error(`Error geocoding address ${itemsData.address}:`, error);
         return null;
       }
     });
