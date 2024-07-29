c0d2daac179683277a89509ee553c577b57d2ec6
Calmer_com <ty.calmer96@gmail.com>
2024-07-29 16:33:16 +0900

    feat: allMark hook도 공통 map hooks 사용 완료

--- src/hooks/useNaverMaps.ts
+++ src/hooks/useNaverMaps.ts
@@ -8,42 +8,28 @@ declare global {
   }
 }
 
-interface UseNaverMapsReturn {
-  map: any;
-  createMarker: (latitude: number, longitude: number) => any;
-}
-
-export const useNaverMaps = (ncpClientId: string): UseNaverMapsReturn => {
-  const [map, setMap] = useState<any>(null);
+export const useNaverMaps = (mapContainerId: string, onMapLoaded: (map: any) => void): void => {
+  const [ncpClientId, setNcpClientId] = useState<string | undefined>(undefined);
 
   useEffect(() => {
+    setNcpClientId(process.env.NEXT_PUBLIC_NCP_CLIENT_ID);
+  }, []);
+
+  useEffect(() => {
+    if (ncpClientId === undefined) return;
+
     const NAVER_MAP_SCRIPT_URL = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${ncpClientId}`;
-    const MARKER_CLUSTERING_SCRIPT_URL = "/libraries/markerClustering.js";
 
     const initMap = async (): Promise<void> => {
-      if (window.naver === undefined) {
+      if (typeof window.naver === "undefined") {
         console.error("네이버 맵 라이브러리가 로드되지 않았습니다.");
         return;
       }
 
-      const map = new window.naver.maps.Map("map", getMapInitialOptions());
-      setMap(map);
+      const map = new window.naver.maps.Map(mapContainerId, getMapInitialOptions());
+      onMapLoaded(map);
     };
-    loadScript(NAVER_MAP_SCRIPT_URL, () => {
-      loadScript(MARKER_CLUSTERING_SCRIPT_URL, initMap);
-    });
-  }, [ncpClientId]);
 
-  const createMarker = (latitude: number, longitude: number): any => {
-    if (map === undefined) return null;
-
-    const markerPosition = new window.naver.maps.LatLng(latitude, longitude);
-    const marker = new window.naver.maps.Marker({
-      position: markerPosition,
-      map,
-    });
-    return marker;
-  };
-
-  return { map, createMarker };
+    loadScript(NAVER_MAP_SCRIPT_URL, initMap);
+  }, [ncpClientId, mapContainerId, onMapLoaded]);
 };
--- src/hooks/useSelectMarkerMaps.ts
+++ src/hooks/useSelectMarkerMaps.ts
@@ -1,40 +1,21 @@
-import { useEffect, useState } from "react";
-import { getMapInitialOptions, loadScript } from "@/src//commons/libraries/utils/naverMaps";
 import type { IGeocodeData } from "../types";
+import { useNaverMaps } from "./useNaverMaps";
 
 export const useSelectMarkerMaps = (props: IGeocodeData | null): void => {
-  const [ncpClientId, setNcpClientId] = useState<string | undefined>(undefined);
+  const onMapLoaded = (map: any): void => {
+    if (props !== null) {
+      const markerPosition = new window.naver.maps.LatLng(props.latitude, props.longitude);
 
-  useEffect(() => {
-    setNcpClientId(process.env.NEXT_PUBLIC_NCP_CLIENT_ID);
-  }, []);
+      // 마커를 변수에 저장하고 이를 활용
+      const marker = new window.naver.maps.Marker({
+        position: markerPosition,
+        map,
+      });
+      marker.setMap(map);
 
-  useEffect(() => {
-    const NAVER_MAP_SCRIPT_URL = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${ncpClientId}`;
-
-    const initMap = async (): Promise<void> => {
-      if (typeof window.naver === "undefined") {
-        console.error("네이버 맵 라이브러리가 로드되지 않았습니다.");
-        return;
-      }
-
-      // 마커를 담을 Map 생성
-      const map = new window.naver.maps.Map("map", getMapInitialOptions());
-
-      if (props !== null) {
-        const markerPosition = new window.naver.maps.LatLng(props.latitude, props.longitude);
-
-        // 마커를 변수에 저장하고 이를 활용
-        const marker = new window.naver.maps.Marker({
-          position: markerPosition,
-          map,
-        });
-        marker.setMap(map);
-
-        // 지도 중심을 마커 위치로 이동
-        map.setCenter(markerPosition);
-      }
-    };
-    loadScript(NAVER_MAP_SCRIPT_URL, initMap);
-  }, [ncpClientId, props]);
+      // 지도 중심을 마커 위치로 이동
+      map.setCenter(markerPosition);
+    }
+  };
+  useNaverMaps("map", onMapLoaded);
 };
