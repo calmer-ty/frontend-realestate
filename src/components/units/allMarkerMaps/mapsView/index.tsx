d6bead0e89fe217aba7c2614583514020aacb275
Calmer_com <ty.calmer96@gmail.com>
2024-08-01 16:48:16 +0900

    feat: 컴포넌트 명칭 변경

--- src/components/units/allMarkerMaps/mapsView/index.tsx
+++ src/components/units/allMarkerMaps/mapsView/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { containerStyle, imgStyle } from "./styles";
 import type { IMapViewProps } from "./types";
 
-export default function MapView(props: IMapViewProps): JSX.Element {
+export default function MapsView(props: IMapViewProps): JSX.Element {
   return (
     <div id="map" css={containerStyle}>
       {props.geocodeResults.length === 0 && <Image alt="loading" src="/images/load.gif" width={60} height={60} css={imgStyle} />}
