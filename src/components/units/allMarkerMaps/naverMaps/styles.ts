dd50a870b9dd4dbb00f20d34b5ff8de2ab8746ff
Calmer_com <ty.calmer96@gmail.com>
2024-08-06 09:39:14 +0900

    feat: 맵 로드스피닝 정렬

--- src/components/units/allMarkerMaps/naverMaps/styles.ts
+++ src/components/units/allMarkerMaps/naverMaps/styles.ts
@@ -2,6 +2,9 @@ import { css } from "@emotion/react";
 import { mediaQueries } from "@/src/commons/styles/styles";
 
 export const containerStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100%;
   flex: 3;
