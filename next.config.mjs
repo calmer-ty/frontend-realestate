df047b01f4b2aca6cd7c9e76fb858b697d55e2c5
Calmer <ty.calmer96@gmail.com>
2024-08-15 15:33:39 +0900

    chore: next js 설정의 파이어베이스 이미지 허용 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   images: {
-    domains: ["firebasestorage.googleapis.com"], // Firebase Storage의 도메인 추가
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "", // 기본적으로 포트는 빈 문자열로 설정
+        pathname: "/**", // 모든 경로를 허용
+      },
+    ],
   },
 };
 
