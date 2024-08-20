9cb9752bfb68f228956d177acd95da95c4da0871
Calmer_com <ty.calmer96@gmail.com>
2024-08-20 15:36:40 +0900

    fix: home 컴포넌트 스타일 수정

--- src/components/units/home/styles.ts
+++ src/components/units/home/styles.ts
@@ -1,6 +1,3 @@
-// import { mediaQueries } from "@/src/commons/styles/styles";
-// import { css } from "@emotion/react";
-
 import styled from "@emotion/styled";
 
 export const Container = styled.article`
@@ -13,18 +10,18 @@ export const Container = styled.article`
   position: relative;
   > section {
     height: 50%;
-    > div {
+    .inner {
       display: flex;
       justify-content: center;
       align-items: center;
       row-gap: 10px;
       height: 100%;
-      padding: 0 20px;
       margin: 0 auto;
       max-width: 1280px;
     }
   }
 `;
+
 export const Maps = styled.section`
   position: relative;
   ::before {
@@ -33,10 +30,10 @@ export const Maps = styled.section`
     width: 100%;
     height: 100%;
     position: absolute;
-    background: #f00 url("/images/main.jpg") no-repeat center/cover;
+    background: #fff url("/images/main.jpg") no-repeat center/cover;
     opacity: 0.2;
   }
-  > div {
+  .inner {
     justify-content: space-between;
     column-gap: 60px;
   }
@@ -49,108 +46,118 @@ export const BuildingType = styled.div`
   background-color: #fff;
   border-radius: 10px;
   cursor: pointer;
-
+  /* 링크 요소 */
   > a {
     display: flex;
     justify-content: center;
     align-items: center;
     width: inherit;
     height: inherit;
+    .textWrap {
+      display: flex;
+      flex-direction: column;
+      row-gap: 10px;
+      width: 160px;
+    }
+    .iconWrap {
+      position: absolute;
+      right: 20px;
+      bottom: 20px;
+    }
   }
   &:hover {
     box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 20px 0px;
   }
 `;
-export const BuildingTypeU = styled.div`
+export const UnBuildingType = styled.div`
   position: relative;
   width: 280px;
   height: 200px;
   background-color: #ccc;
   border-radius: 10px;
   cursor: pointer;
-
+  /* 링크 요소 */
   > a {
     display: flex;
     justify-content: center;
     align-items: center;
     width: inherit;
     height: inherit;
+    .textWrap {
+      display: flex;
+      flex-direction: column;
+      row-gap: 10px;
+      width: 160px;
+    }
+    .iconWrap {
+      position: absolute;
+      right: 20px;
+      bottom: 20px;
+    }
   }
   &:hover {
     box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 20px 0px;
   }
 `;
 
-export const TextWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 10px;
-  width: 160px;
-`;
-export const IconWrap = styled.div`
-  position: absolute;
-  right: 20px;
-  bottom: 20px;
-`;
-
 export const Registered = styled.section`
-  background-color: #fff;
-  > div {
+  .inner {
     flex-direction: column;
     > h2 {
       width: 100%;
     }
-    > ul {
-      display: flex;
-      column-gap: 30px;
-      width: 100%;
-      height: 300px;
-    }
+  }
+  .contents {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 300px;
   }
 `;
-export const RegisteredItem = styled.li`
-  > a {
-    display: flex;
-    flex-direction: column;
-    row-gap: 10px;
-    /* 이미지 랩 */
-    div.imageWrap {
-      width: 300px;
-      height: 200px;
-      position: relative;
-      > img {
-        object-fit: cover;
-      }
-      ::before {
-        content: "";
-        display: none;
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        background-color: #000;
-        opacity: 0.3;
-      }
-    }
-    > p.buildingDesc {
+export const RegisteredList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  > li {
+    > a {
       display: flex;
       flex-direction: column;
-      > strong {
-        font-size: 20px;
-        margin-bottom: 10px;
-      }
-    }
-    :hover {
+      row-gap: 10px;
+      /* 이미지 랩 */
       .imageWrap {
+        width: 300px;
+        height: 200px;
+        position: relative;
+        > img {
+          object-fit: cover;
+        }
         ::before {
-          display: block;
+          content: "";
+          display: none;
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          background-color: #000;
+          opacity: 0.3;
+          z-index: 1;
+        }
+      }
+      > p.buildingDesc {
+        display: flex;
+        flex-direction: column;
+        > strong {
+          font-size: 20px;
+          margin-bottom: 10px;
+        }
+      }
+      :hover {
+        .imageWrap {
+          ::before {
+            display: block;
+          }
         }
       }
     }
-  }
-`;
-
-export const Option = styled.section`
-  > div {
-    flex-direction: column;
   }
 `;
