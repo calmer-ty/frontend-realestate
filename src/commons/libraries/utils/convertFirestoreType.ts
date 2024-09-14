3935150fa31b66795fca3cce7489b6ce1da73a22
Calmer <ty.calmer96@gmail.com>
2024-09-14 14:01:02 +0900

    refactor: firestore type 컨버터 유틸로 추가

---
+++ src/commons/libraries/utils/convertFirestoreType.ts
@@ -0,0 +1,30 @@
+import type { DocumentData } from "firebase/firestore";
+import type { IFirebaseData } from "../../types";
+
+export const convertFirebaseData = (data: DocumentData): IFirebaseData => {
+  return {
+    _id: data._id as string,
+    type: data.type as string,
+    address: data.address as string,
+    addressDetail: data.addressDetail as string,
+    floor: data.floor as number,
+    area: data.area as number,
+    price: data.price as number,
+    roomCount: data.roomCount as number,
+    bathroomCount: data.bathroomCount as number,
+    imageUrls: data.imageUrls as string[],
+    manageCost: data.number as number,
+    desc: data.desc as string,
+    // prettier-ignore
+    user: data.user !== undefined ? {
+      _id: data.user._id as string,
+      name: data.user.name as string,
+      email: data.user.email as string,
+    }: undefined,
+    // prettier-ignore
+    createdAt: data.createdAt !== undefined ? {
+      seconds: data.seconds as  number,
+      nanoseconds: data.nanoseconds as  number,
+    }: undefined,
+  };
+};
--- src/hooks/firebase/useFirebase.ts
+++ src/hooks/firebase/useFirebase.ts
@@ -1,37 +1,9 @@
 import { useCallback } from "react";
 import { db } from "@/src/commons/libraries/firebase/firebaseApp";
 import { addDoc, collection, doc, getDoc, getDocs, updateDoc } from "firebase/firestore";
-import type { DocumentData } from "firebase/firestore";
+import { convertFirebaseData } from "@/src/commons/libraries/utils/convertFirestoreType";
 import type { IFirebaseData, IUseFirebaseProps } from "@/src/commons/types";
 import type { IWriteFormData } from "@/src/components/units/buildings/write/types";
-
-const convertFirebaseData = (data: DocumentData): IFirebaseData => {
-  return {
-    _id: data._id as string,
-    type: data.type as string,
-    address: data.address as string,
-    addressDetail: data.addressDetail as string,
-    floor: data.floor as number,
-    area: data.area as number,
-    price: data.price as number,
-    roomCount: data.roomCount as number,
-    bathroomCount: data.bathroomCount as number,
-    imageUrls: data.imageUrls as string[],
-    manageCost: data.number as number,
-    desc: data.desc as string,
-    // prettier-ignore
-    user: data.user !== undefined ? {
-      _id: data.user._id as string,
-      name: data.user.name as string,
-      email: data.user.email as string,
-    }: undefined,
-    // prettier-ignore
-    createdAt: data.createdAt !== undefined ? {
-      seconds: data.seconds as  number,
-      nanoseconds: data.nanoseconds as  number,
-    }: undefined,
-  };
-};
 
 export const useFirebase = (): IUseFirebaseProps => {
   const createFirebaseData = useCallback(async (data: IWriteFormData, selectedType: string): Promise<void> => {
@@ -56,7 +28,7 @@ export const useFirebase = (): IUseFirebaseProps => {
       const docSnap = await getDoc(docRef);
       if (docSnap.exists()) {
         const data = docSnap.data();
-        if (data !== null && data !== undefined) {
+        if (data != null) {
           return convertFirebaseData(data);
         }
       }
