dc496790b26ff7a535fe67c78f4021684bb9bfe9
Calmer <ty.calmer96@gmail.com>
2024-10-06 16:18:34 +0900

    docs: api 설명 추가

--- src/commons/libraries/region/regionApi.ts
+++ src/commons/libraries/region/regionApi.ts
@@ -5,7 +5,14 @@ const API_KEY = process.env.NEXT_PUBLIC_GOVERNMENT_PUBLIC_DATA;
 
 export const regionApi = async (city: string): Promise<IRegionData> => {
   // 캐시에 없는 경우 실제 데이터를 요청합니다
-  const reginCdUrl = `http://apis.data.go.kr/1741000/StanReginCd/getStanReginCdList?ServiceKey=${API_KEY}&type=json&pageNo=1&numOfRows=300&flag=Y&locatadd_nm=${encodeURIComponent(city)}`;
-  const response = await axios.get(reginCdUrl);
-  return response.data;
+  const reginCdUrl = `http://apis.data.go.kr/1741000/StanReginCd/getStanReginCdList?ServiceKey=${API_KEY}&type=json&pageNo=1&numOfRows=10&flag=Y&locatadd_nm=${encodeURIComponent(city)}`;
+
+  try {
+    const response = await axios.get(reginCdUrl);
+
+    return response.data;
+  } catch (error) {
+    console.error("API 요청 중 에러 발생:", error);
+    throw error;
+  }
 };
