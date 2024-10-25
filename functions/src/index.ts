78ff3989397a64a6762dc578030af6257504c6ee
Calmer_com <ty.calmer96@gmail.com>
2024-10-25 12:20:21 +0900

    chore: 배포설정 추가

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,12 +8,27 @@
  */
 
 import { onRequest } from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+// import * as logger from "firebase-functions/logger";
+
+// 추가
+import next from "next";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", { structuredData: true });
+//   response.send("Hello from Firebase!");
+// });
+
+// 추가
+const nextjsServer = next({
+  dev: false, // 배포 환경에서는 false로 설정
+  conf: { distDir: ".next" }, // Next.js 빌드 파일 디렉토리
+});
+
+const handle = nextjsServer.getRequestHandler();
+
+export const nextjsFunc = onRequest((req, res) => {
+  return nextjsServer.prepare().then(() => handle(req, res));
 });
