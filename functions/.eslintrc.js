3b37483e8f13f41faaeaeaae9b86a3610583b5cd
Calmer <ty.calmer96@gmail.com>
2024-10-28 23:12:23 +0900

    chore: firebase functions - CLI 업데이트, 룰 추가

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -30,6 +30,8 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    // 추가
     "object-curly-spacing": ["error", "always"],
+    "linebreak-style": ["error", "windows"],
   },
 };
