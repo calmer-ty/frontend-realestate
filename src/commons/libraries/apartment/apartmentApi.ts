6ef1a6b3fa493995a9c10bfa9fa7365b4e2cfc3e
Calmer_com <ty.calmer96@gmail.com>
2024-11-20 12:20:07 +0900

    fix: 토탈데이터값을 지정해주는 작업이 필요

--- src/commons/libraries/apartment/apartmentApi.ts
+++ src/commons/libraries/apartment/apartmentApi.ts
@@ -7,9 +7,9 @@ const API_KEY = process.env.NEXT_PUBLIC_GOVERNMENT_PUBLIC_DATA;
 export const apartmentApi = async (result: IRegionItem): Promise<IApartment> => {
   const currentDate = getCurrentDate();
   // 캐시에 없는 경우 실제 데이터를 요청합니다
-  // const apartmentUrl = `http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?LAWD_CD=${result.region_cd}&DEAL_YMD=${currentDate}&serviceKey=${API_KEY}`;
-  const apartmentUrl = `https://apis.data.go.kr/1613000/RTMSDataSvcAptTrade/getRTMSDataSvcAptTrade?serviceKey=${API_KEY}&LAWD_CD=${result.region_cd}&DEAL_YMD=${currentDate}&pageNo=1&numOfRows=5`;
+  const apartmentUrl = `https://apis.data.go.kr/1613000/RTMSDataSvcAptTrade/getRTMSDataSvcAptTrade?serviceKey=${API_KEY}&LAWD_CD=${result.region_cd}&DEAL_YMD=${currentDate}&pageNo=1&numOfRows=50`;
   const response = await axios.get(apartmentUrl);
+  console.log("apartmentApi ==== ", response.data);
 
   return response.data;
 };
--- src/commons/libraries/region/regionApi.ts
+++ src/commons/libraries/region/regionApi.ts
@@ -6,23 +6,28 @@ const baseUrl = `http://apis.data.go.kr/1741000/StanReginCd/getStanReginCdList`;
 
 export const regionApi = async (city: string): Promise<IRegion> => {
   // 캐시에 없는 경우 실제 데이터를 요청합니다
-  // const reginCdUrl = `${baseUrl}?ServiceKey=${API_KEY}&type=json&pageNo=1&numOfRows=10&flag=Y&locatadd_nm=${encodeURIComponent(city)}`;
-  const reginCdUrl = `${baseUrl}?ServiceKey=${API_KEY}&type=json&pageNo=1&numOfRows=10&flag=Y&locatadd_nm=${encodeURIComponent(city)}`;
+  const reginCdUrl = `${baseUrl}?ServiceKey=${API_KEY}&type=json&pageNo=1&numOfRows=50&flag=Y&locatadd_nm=${encodeURIComponent(city)}`;
   try {
     // 첫 번째 API 호출
     // totalCount 가져오기 - totalCount를 알려면, api를 먼저 한번 실행해보아야 한다.
     const response = await axios.get<IRegion>(reginCdUrl);
-    const totalCount = Math.round(response?.data?.StanReginCd?.[0]?.head?.[0]?.totalCount ?? 0);
-    // console.log("totalCount::: ", totalCount);
-    console.log("city", city);
-    console.log("response?.data::: ", response?.data.StanReginCd?.[0]);
+    // const totalCount = Math.round(response?.data?.StanReginCd?.[0]?.head?.[0]?.totalCount ?? 0);
+    // console.log("city", city);
+    // console.log("response?.data::: ", response?.data.StanReginCd?.[0]);
 
     // 두 번째 API 호출
     // const finalReginCdUrl = `${baseUrl}?ServiceKey=${API_KEY}&type=json&pageNo=1&numOfRows=${totalCount}&flag=Y&locatadd_nm=${encodeURIComponent(city)}`;
-    const finalReginCdUrl = `${baseUrl}?ServiceKey=${API_KEY}&type=json&pageNo=1&numOfRows=${totalCount}&flag=Y&locatadd_nm=${encodeURIComponent(city)}`;
-    const finalResponse = await axios.get<IRegion>(finalReginCdUrl);
+    // const finalReginCdUrl = `${baseUrl}?ServiceKey=${API_KEY}&type=json&pageNo=1&numOfRows=10&flag=Y&locatadd_nm=${encodeURIComponent(city)}`;
+    // const finalResponse = await axios.get<IRegion>(finalReginCdUrl);
 
-    return finalResponse.data;
+    // console.log("city", city);
+    // console.log("finalResponse data::: ", finalResponse?.data);
+    // console.log("finalResponse StanReginCd::: ", finalResponse?.data.StanReginCd);
+    // console.log("===== ERROR 1 =====", finalResponse?.data);
+    // console.log("===== ERROR 2 =====", finalResponse?.data?.RESULT.resultCode);
+    // console.log("==========");
+
+    return response.data;
   } catch (error) {
     console.error("API 요청 중 에러 발생:", error);
     throw error;
