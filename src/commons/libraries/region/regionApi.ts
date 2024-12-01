cefede4a4955559c29ef5270639aba2fb1900df9
Calmer <ty.calmer96@gmail.com>
2024-12-01 12:05:43 +0900

    feat: 지역코드값 페이지 값까지 불러옴

--- src/commons/libraries/region/regionApi.ts
+++ src/commons/libraries/region/regionApi.ts
@@ -6,14 +6,12 @@ const baseUrl = `http://apis.data.go.kr/1741000/StanReginCd/getStanReginCdList`;
 
 export const regionApi = async (city: string): Promise<IRegion> => {
   // ============================================== 테스트용
-  const reginCdUrlTest = `${baseUrl}?ServiceKey=${API_KEY}&type=json&flag=Y&locatadd_nm=${encodeURIComponent("세종특별자치시")}`;
+  const reginCdUrlTest = `${baseUrl}?ServiceKey=${API_KEY}&type=json&flag=Y&locatadd_nm=${encodeURIComponent("서울특별시")}`;
   const numOfRows = 10;
   const delay = (ms: number): Promise<unknown> => new Promise((resolve) => setTimeout(resolve, ms));
   try {
-    console.time("Total Request Time");
-    console.time("Initial Request");
     const initialResponse = await axios.get<IRegion>(`${reginCdUrlTest}&pageNo=1&numOfRows=${numOfRows}`);
-    console.timeEnd("Initial Request");
+
     const totalCount = initialResponse.data.StanReginCd?.[0]?.head?.[0].totalCount; // row 데이터 추출
 
     if (totalCount === undefined) {
@@ -29,20 +27,25 @@ export const regionApi = async (city: string): Promise<IRegion> => {
       console.time(`Request for Page ${pageNo}`);
       const reginCdUrl2 = `${reginCdUrlTest}&pageNo=${pageNo}&numOfRows=${numOfRows}`;
       requests.push(axios.get<IRegion>(reginCdUrl2)); // 각 요청을 배열에 추가
-      await delay(500); // 500ms의 딜레이를 주어 요청을 천천히 처리하도록 합니다
+      await delay(200); // 500ms의 딜레이를 주어 요청을 천천히 처리하도록 합니다
     }
 
     try {
       // 병렬로 모든 요청을 보내고 응답을 기다림
       const responses = await Promise.all(requests);
 
+      // console.log(`[DEBUG] responses`, responses);
       responses.forEach((response, index) => {
         console.log(`[DEBUG] response.data === PageNo.${index + 1}`, response.data);
+
+        const rows = response.data.StanReginCd?.[1].row;
+        rows?.forEach((row) => {
+          console.log(row.region_cd?.slice(0, 5), row.locatadd_nm);
+        });
       });
     } catch (error) {
       console.error("병렬 요청 중 에러 발생:", error);
     }
-    // console.log("[DEBUG] requests ", requests);
 
     // ============================================== 테스트용
     const reginCdUrl = `${baseUrl}?ServiceKey=${API_KEY}&type=json&pageNo=1&numOfRows=10&flag=Y&locatadd_nm=${encodeURIComponent(city)}`;
