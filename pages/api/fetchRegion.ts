b70ff2bac1ea101edaba05a777621794acdae217
Calmer_com <ty.calmer96@gmail.com>
2024-12-18 14:03:11 +0900

    fix: api 함수 임포트 수정

--- pages/api/fetchRegion.ts
+++ pages/api/fetchRegion.ts
@@ -1,9 +1,9 @@
-import { getAllRegionData } from "@/src/commons/libraries/region/regionData";
+import { getRegionData } from "@/src/commons/libraries/region/regionData";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function fetchRegion(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   try {
-    const data = await getAllRegionData();
+    const data = await getRegionData();
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Error fetching region data" });
