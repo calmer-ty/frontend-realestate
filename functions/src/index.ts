e023703dbbdd7440e4962361043b3ac2dd6ce5dc
Calmer_com <ty.calmer96@gmail.com>
2024-12-19 13:24:57 +0900

    chore: 서버에 이미지 보이지 않는 현상을 위한 설정 수정중

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,10 +23,6 @@ import { onRequest } from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
 import next from "next";
 
-// 호스팅 파일 서빙
-import express from "express";
-import path from "path";
-
 admin.initializeApp();
 
 const isDev = process.env.NODE_ENV !== "production";
@@ -37,15 +33,7 @@ const nextServer = next({
 
 const handle = nextServer.getRequestHandler();
 
-// ===== 호스팅 파일 서빙 =====
-// Express 앱 설정
-const app = express();
-// public 폴더에서 정적 파일 제공
-app.use(express.static(path.join(__dirname, "public")));
-// Next.js 핸들러 설정
-app.all("*", (req, res) => handle(req, res));
-
 export const nextjsFunc = onRequest(async (req, res) => {
   await nextServer.prepare();
-  app(req, res);
+  handle(req, res);
 });
