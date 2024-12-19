e92317ad04451d0dd66a0d987635b11709bfe7ba
Calmer_com <ty.calmer96@gmail.com>
2024-12-19 12:18:00 +0900

    chore: 파이어베이스 서버에 public 폴더 추가 및 설정 변경

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -23,6 +23,10 @@ import { onRequest } from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
 import next from "next";
 
+// 호스팅 파일 서빙
+import express from "express";
+import path from "path";
+
 admin.initializeApp();
 
 const isDev = process.env.NODE_ENV !== "production";
@@ -33,7 +37,15 @@ const nextServer = next({
 
 const handle = nextServer.getRequestHandler();
 
+// ===== 호스팅 파일 서빙 =====
+// Express 앱 설정
+const app = express();
+// public 폴더에서 정적 파일 제공
+app.use(express.static(path.join(__dirname, "public")));
+// Next.js 핸들러 설정
+app.all("*", (req, res) => handle(req, res));
+
 export const nextjsFunc = onRequest(async (req, res) => {
   await nextServer.prepare();
-  handle(req, res);
+  app(req, res);
 });
