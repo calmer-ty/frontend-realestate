856bb5afe5ad442abf4935c3dab447ba47fbcea0
Calmer_com <ty.calmer96@gmail.com>
2025-01-03 13:44:26 +0900

    fix: 컴포넌트 합침

--- src/components/units/buildings/view/mapsInfo/visibleArea/markerList/index.tsx
+++ src/components/units/buildings/view/mapsInfo/visibleArea/markerList/index.tsx
@@ -1,12 +1,35 @@
-import ListItem from "./listItem";
+import { getReduceCityName } from "@/src/commons/libraries/utils/convertCityName";
+import { isBillion, isTenMillion } from "@/src/commons/libraries/utils/priceFormatter";
+import { DEFAULT_STRING_VALUE } from "@/src/commons/constants";
+
 import type { IMarkerListProps } from "./types";
 
 export default function MarkerList({ visibleMarkerData, firestoreData, onClickInfo }: IMarkerListProps): JSX.Element {
   return (
     <ul>
-      {visibleMarkerData.map((el, index) => (
-        <ListItem key={`${el.data?.aptNm}_${index}`} el={el} index={index} firestoreData={firestoreData} onClickInfo={onClickInfo} />
-      ))}
+      {visibleMarkerData.map((el, index) => {
+        const matchingFirestoreData = firestoreData.some((firestoreData) => getReduceCityName(el.geocode?.jibunAddress ?? DEFAULT_STRING_VALUE) === firestoreData.address);
+
+        return (
+          <li
+            key={`${el.data?.aptNm}_${index}`}
+            onClick={() => {
+              onClickInfo(el);
+            }}
+          >
+            <h2>
+              매매 {isBillion(el.data?.dealAmount?.replace(/,/g, "") ?? DEFAULT_STRING_VALUE)}&nbsp;
+              {isTenMillion(el.data?.dealAmount?.replace(/,/g, "") ?? DEFAULT_STRING_VALUE)}원
+            </h2>
+            <p>
+              아파트・{el.data?.aptNm}
+              <br />
+              {el.data?.excluUseAr}m² {el.data?.floor}층
+            </p>
+            <div>{matchingFirestoreData && <>매물있음</>}</div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
--- src/components/units/buildings/view/mapsInfo/visibleArea/markerList/listItem/types.ts
+++
@@ -1,8 +0,0 @@
-import type { IFirestore, IGeocodeData } from "@/src/commons/types";
-
-export interface IVisibleAreaListItemProps {
-  el: IGeocodeData;
-  index: number;
-  firestoreData: IFirestore[];
-  onClickInfo: (el: IGeocodeData) => void;
-}
