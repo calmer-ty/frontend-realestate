c858d054583c68c74180fa8833a781745790303d
Calmer_com <ty.calmer96@gmail.com>
2025-01-06 14:15:59 +0900

    fix: 디테일 페이지 조회 기능, 훅으로 사용

--- app/[buildingType]/[listId]/page.tsx
+++ app/[buildingType]/[listId]/page.tsx
@@ -1,44 +1,37 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "@/src/commons/libraries/firebase/firebaseApp";
+import { useFirestore } from "@/src/hooks/firebase/useFirestore";
+
 import BuildingDetail from "@/src/components/units/buildings/detail";
-import LoadingSpinner from "@/src/components/commons/loadingSpinner";
 import type { IBuildingListParams, IFirestore } from "@/src/commons/types";
 
 export default function BuildingDetailPage({ params }: { params: IBuildingListParams }): JSX.Element {
-  const { buildingType, listId } = params;
-  const [buildingData, setBuildingData] = useState<IFirestore | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+  const { listId } = params;
+  const [buildings, setBuildings] = useState<IFirestore | null>(null);
+  const { readFirestore } = useFirestore();
 
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
       try {
-        const docRef = doc(db, buildingType, listId);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-          setBuildingData(docSnap.data() as IFirestore);
+        // readFirestores를 통해 Firestore에서 데이터 가져오기
+        const buildings = await readFirestore("buildings", listId);
+        if (buildings !== undefined) {
+          setBuildings(buildings);
         } else {
           console.log("No such document!");
         }
       } catch (error) {
         console.error("Error fetching documents: ", error);
-      } finally {
-        setLoading(false);
       }
     };
+
     void fetchData();
-  }, [buildingType, listId]);
+  }, [listId, readFirestore]);
 
-  if (loading) {
-    return <LoadingSpinner size={100} />;
-  }
-
-  if (buildingData === null) {
+  if (buildings === null) {
     return <div>Error fetching document.</div>;
   }
 
-  return <BuildingDetail buildingData={buildingData} />;
+  return <BuildingDetail buildingData={buildings} />;
 }
