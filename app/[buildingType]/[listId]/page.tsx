2ac02f508dfbba8fa2b13ca8d651ee48d6d2e404
Calmer <ty.calmer96@gmail.com>
2025-01-07 21:37:01 +0900

    feat: 디테일 페이지 로딩시 로드스피너 추가

--- app/[buildingType]/[listId]/page.tsx
+++ app/[buildingType]/[listId]/page.tsx
@@ -5,10 +5,12 @@ import { useFirestore } from "@/src/hooks/firebase/useFirestore";
 
 import BuildingDetail from "@/src/components/units/buildings/detail";
 import type { IBuildingListParams, IFirestore } from "@/src/commons/types";
+import LoadingSpinner from "@/src/components/commons/loadingSpinner";
 
 export default function BuildingDetailPage({ params }: { params: IBuildingListParams }): JSX.Element {
   const { listId } = params;
   const [buildings, setBuildings] = useState<IFirestore | null>(null);
+  const [loading, setLoading] = useState(true); // 로딩 상태 추가
   const { readFirestore } = useFirestore();
 
   useEffect(() => {
@@ -22,12 +24,18 @@ export default function BuildingDetailPage({ params }: { params: IBuildingListPa
           console.log("No such document!");
         }
       } catch (error) {
-        console.error("Error fetching documents: ", error);
+        console.error("Error fetching document: ", error);
+      } finally {
+        setLoading(false); // 로딩 상태 완료
       }
     };
 
     void fetchData();
   }, [listId, readFirestore]);
+
+  if (loading) {
+    return <LoadingSpinner size={100} />; // 로딩 상태 표시
+  }
 
   if (buildings === null) {
     return <div>Error fetching document.</div>;
