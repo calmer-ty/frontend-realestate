f9849e3755adc5a85281d58a6c2d9506a6a31a23
Calmer_com <ty.calmer96@gmail.com>
2025-01-07 16:42:07 +0900

    fix: 데이터 로딩시 분기처리

--- src/components/units/buildings/view/naverMaps/index.tsx
+++ src/components/units/buildings/view/naverMaps/index.tsx
@@ -4,9 +4,5 @@ import type { INaverMapsProps } from "./types";
 import * as S from "./styles";
 
 export default function NaverMaps(props: INaverMapsProps): JSX.Element {
-  return (
-    <S.Maps id="map">
-      <LoadingSpinner size={100} />
-    </S.Maps>
-  );
+  return <>{props.loading ? <LoadingSpinner size={100} /> : <S.Maps id="map"></S.Maps>}</>;
 }
