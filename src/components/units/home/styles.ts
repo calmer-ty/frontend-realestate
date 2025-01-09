cb223e994674f4b879b6721fb297cd39b702b4cb
Calmer_com <ty.calmer96@gmail.com>
2025-01-09 17:42:44 +0900

    feat: 메인 페이지 이미지 최적화

--- src/components/units/home/styles.ts
+++ src/components/units/home/styles.ts
@@ -1,45 +1,8 @@
 import styled from "@emotion/styled";
-import { mediaQueries } from "@/src/commons/styles";
-import { css } from "@emotion/react";
 
 export const Container = styled.article`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100%;
-`;
-
-// 맵 선택지
-export const MapOptions = styled.section`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  height: 50%;
-
-  .inner {
-    display: flex;
-    justify-content: space-between;
-    width: 1024px;
-    padding: 20px;
-  }
-  /* 가상요소 */
-  ::before {
-    content: "";
-    display: block;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    background: #fff url("/images/main.jpg") no-repeat center/cover;
-    opacity: 0.2;
-  }
-  /* 반응형 */
-  ${mediaQueries.mobile(css`
-    height: 100%;
-    .inner {
-      flex-direction: column;
-      align-items: center;
-      row-gap: 40px;
-    }
-  `)}
 `;
