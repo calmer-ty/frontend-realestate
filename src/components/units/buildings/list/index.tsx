c64660f4472580838a4d123e683e82ba3654dbd3
Calmer <ty.calmer96@gmail.com>
2025-01-18 11:59:04 +0900

    refactor: list 컴포넌트 훅 정리

--- src/components/units/buildings/list/index.tsx
+++ src/components/units/buildings/list/index.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import { useUserBuildings } from "./hooks/useUserBuildings";
-import { useDeleteModal } from "./hooks/useDeleteModal";
+import { useCallback, useEffect, useState } from "react";
+import { useAuth } from "@/src/hooks/useAuth";
 import { useFirestore } from "@/src/hooks/firebase/useFirestore";
 
 import DeleteModal from "./deleteModal";
@@ -14,11 +13,46 @@ import type { IFirestore } from "@/src/commons/types";
 export default function BuildingList(): JSX.Element {
   const [buildings, setBuildings] = useState<IFirestore[]>([]);
   const [deletedBuildings, setDeletedBuildings] = useState<IFirestore[]>([]);
-
   const { archiveFirestore, deleteFirestore, readFirestores } = useFirestore();
-  const { loading } = useUserBuildings({ setBuildings, setDeletedBuildings, readFirestores });
 
-  const { modalOpen, setModalOpen, selectedBuilding, onDeleteModalOpen } = useDeleteModal();
+  const { user } = useAuth();
+  const userId = user?.uid;
+  const [loading, setLoading] = useState(true);
+  const fetchBuildings = useCallback(async (): Promise<void> => {
+    try {
+      setLoading(true);
+      // Firebase에서 아파트, 집, 삭제된 아파트 데이터를 가져옴
+      const buildings = await readFirestores("buildings");
+      const deletedBuildings = await readFirestores("deleted_buildings");
+
+      // 유저 ID에 맞는 건물 필터링
+      const userBuildings = buildings.filter((el) => el.user?._id === userId);
+      const userDeletedBuildings = deletedBuildings.filter((el) => el.user?._id === userId);
+
+      // 상태 업데이트: 유저 데이터만 설정
+      setBuildings(userBuildings);
+      setDeletedBuildings(userDeletedBuildings);
+    } catch (error) {
+      console.error("Error fetching buildings:", error);
+    } finally {
+      setLoading(false); // 데이터 로딩 완료
+    }
+  }, [userId, setBuildings, setDeletedBuildings, readFirestores]);
+  useEffect(() => {
+    if (userId !== undefined) {
+      // 컴포넌트가 마운트될 때 데이터 한 번만 불러옴
+      void fetchBuildings();
+    }
+  }, [userId, fetchBuildings]);
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedBuilding, setSelectedBuilding] = useState<IFirestore | null>(null);
+
+  const onDeleteModalOpen = (building: IFirestore): void => {
+    setSelectedBuilding(building); // 클릭된 매물 데이터 저장
+    setModalOpen(true); // 모달 열기
+  };
+
   return (
     <>
       <S.Container>
