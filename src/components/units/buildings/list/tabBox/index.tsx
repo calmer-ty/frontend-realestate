c2a7a875db8e81fa571a23c06c5e51748aa3c829
Calmer_com <ty.calmer96@gmail.com>
2025-01-21 12:14:50 +0900

    fix: 광고 종료된 매물은 포인트 안잡히게 수정

--- src/components/units/buildings/list/tabBox/index.tsx
+++ src/components/units/buildings/list/tabBox/index.tsx
@@ -86,7 +86,7 @@ export default function TabBox(props: ITabBoxProps): JSX.Element {
           ) : (
             <ul>
               {sortedMyDeletedBuildings.map((el, index) => (
-                <ListItem key={`${el._id}_${index}`} el={el} index={index} onDeleteModalOpen={onDeleteModalOpen} isDeleted={false} />
+                <ListItem key={`${el._id}_${index}`} el={el} index={index} onDeleteModalOpen={onDeleteModalOpen} isDeleted={true} />
               ))}
             </ul>
           )}
