f92280c235da6c13ff1a27d1f20e2ae509eb2dc8
Calmer_com <ty.calmer96@gmail.com>
2025-01-22 16:38:45 +0900

    feat: 홈페이지 컴포넌트에 오픈그래프 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,10 +13,6 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="부동산 웹 어플리케이션" />
-        <meta property="og:description" content="거래된 매물을 조회하고, 직접 매물을 등록해보세요." />
-        <meta property="og:url" content="https://www.calmer96.store" />
-        <meta property="og:image" content="/images/main.jpg" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
