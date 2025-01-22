7aacaeccf7d524b18334583ce371d9d39ae835f7
Calmer_com <ty.calmer96@gmail.com>
2025-01-22 13:59:32 +0900

    feat: 파비콘 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
     <html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="public/favicon.ico" />
       </Head>
       <body>
         <div id="wrapper">
