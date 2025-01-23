035642b7aa049c712fb45444e3fa449e455a7594
Calmer_com <ty.calmer96@gmail.com>
2025-01-23 12:42:23 +0900

    fix: params 비동기 방식 방법으로 수정

--- app/[buildingType]/[listId]/page.tsx
+++ app/[buildingType]/[listId]/page.tsx
@@ -7,9 +7,23 @@ import BuildingDetail from "@/src/components/units/buildings/detail";
 import LoadingSpinner from "@/src/components/commons/loadingSpinner";
 
 import type { IBuildingListParams, IFirestore } from "@/src/commons/types";
+interface IBuildingListParamsProps {
+  params: Promise<IBuildingListParams>;
+}
 
-export default function BuildingDetailPage({ params }: { params: IBuildingListParams }): JSX.Element {
-  const { listId } = params;
+export default function BuildingDetailPage({ params }: IBuildingListParamsProps): JSX.Element {
+  const [listId, setListId] = useState<string | null>(null);
+
+  // params를 비동기적으로 처리하려면 await로 기다려야 함
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const resolvedParams = await params;
+      setListId(resolvedParams.listId);
+    };
+
+    void fetchData();
+  }, [params]);
+
   const [buildings, setBuildings] = useState<IFirestore | null>(null);
   const [loading, setLoading] = useState(true); // 로딩 상태 추가
   const { readFirestore } = useFirestore();
@@ -17,6 +31,8 @@ export default function BuildingDetailPage({ params }: { params: IBuildingListPa
   useEffect(() => {
     const fetchData = async (): Promise<void> => {
       try {
+        if (listId === null) return;
+
         // readFirestores를 통해 Firestore에서 데이터 가져오기
         const buildings = await readFirestore("buildings", listId);
         if (buildings !== undefined) {
--- app/[buildingType]/page.tsx
+++ app/[buildingType]/page.tsx
@@ -1,7 +1,30 @@
-import BuildingView from "@/src/components/units/buildings/view";
-import type { IBuildingParams } from "@/src/commons/types";
+import { useEffect, useState } from "react";
 
-export default function BuildingsPage({ params }: { params: IBuildingParams }): JSX.Element {
-  const buildingType = params.buildingType; // URL 파라미터 추출
+import BuildingView from "@/src/components/units/buildings/view";
+import LoadingSpinner from "@/src/components/commons/loadingSpinner";
+
+import type { IBuildingParams } from "@/src/commons/types";
+interface IBuildingParamsProps {
+  params: Promise<IBuildingParams>;
+}
+
+export default function BuildingsPage({ params }: IBuildingParamsProps): JSX.Element {
+  // const buildingType = params.buildingType; // URL 파라미터 추출
+  const [buildingType, setBuildingType] = useState<string | null>(null);
+
+  // params를 비동기적으로 처리하려면 await로 기다려야 함
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      const resolvedParams = await params;
+      setBuildingType(resolvedParams.buildingType);
+    };
+
+    void fetchData();
+  }, [params]);
+
+  if (buildingType === null) {
+    return <LoadingSpinner size={100} />;
+  }
+
   return <BuildingView buildingType={buildingType} />;
 }
