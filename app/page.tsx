dd3b9c51be342ae39d1ac833713821125d5813ce
Calmer_com <ty.calmer96@gmail.com>
2025-01-23 13:35:30 +0900

    feat: generateMetadata 홈에 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,31 @@
-"use client";
-
 import Home from "@/src/components/units/home";
-import Head from "next/head";
+
+// metaData
+interface IOpenGraphMetadata {
+  title: string;
+  description: string;
+  url: string;
+  images: string[];
+}
+interface IMetadata {
+  title: string;
+  description: string;
+  openGraph: IOpenGraphMetadata;
+}
+
+export const generateMetadata = async (): Promise<IMetadata> => {
+  return {
+    title: "부동산 웹 어플리케이션",
+    description: "거래된 매물을 조회하고, 직접 매물을 등록해보세요.",
+    openGraph: {
+      title: "부동산 웹 어플리케이션",
+      description: "거래된 매물을 조회하고, 직접 매물을 등록해보세요.",
+      url: "/images/main.jpg",
+      images: ["/images/main.jpg"],
+    },
+  };
+};
 
 export default function HomePage(): JSX.Element {
-  return (
-    <>
-      <Head>
-        <meta property="og:title" content="부동산 웹 어플리케이션" />
-        <meta property="og:description" content="거래된 매물을 조회하고, 직접 매물을 등록해보세요." />
-        <meta property="og:url" content="/images/main.jpg" />
-        <meta property="og:image" content="https://www.calmer96.store" />
-      </Head>
-      <Home />
-    </>
-  );
+  return <Home />;
 }
