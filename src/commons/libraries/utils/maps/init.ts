0391039071d233889c24d6d5825e9194c5c80283
Calmer_com <ty.calmer96@gmail.com>
2025-02-05 14:19:28 +0900

    fix: 맵 줌컨트롤 삭제

--- src/commons/libraries/utils/maps/init.ts
+++ src/commons/libraries/utils/maps/init.ts
@@ -2,10 +2,10 @@ interface IGetMapInitOptionsProps {
   center: any;
   zoom: number;
   zoomControl: boolean;
-  zoomControlOptions: {
-    position: any;
-    style: any;
-  };
+  // zoomControlOptions: {
+  //   position: any;
+  //   style: any;
+  // };
 }
 
 export const loadScript = (src: string, onLoad: () => void): void => {
@@ -22,8 +22,8 @@ export const getMapInitOptions: () => IGetMapInitOptionsProps = () => ({
   center: new window.naver.maps.LatLng(37.38282, 127.118926),
   zoom: 15,
   zoomControl: true,
-  zoomControlOptions: {
-    position: window.naver.maps.Position.BOTTOM_LEFT,
-    style: window.naver.maps.ZoomControlStyle.SMALL,
-  },
+  // zoomControlOptions: {
+  //   position: window.naver.maps.Position.BOTTOM_LEFT,
+  //   style: window.naver.maps.ZoomControlStyle.SMALL,
+  // },
 });
