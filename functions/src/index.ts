6ae8e58b57b41c2b9de05c30fa9bcfdd0a468ac7
Calmer_com <ty.calmer96@gmail.com>
2025-02-13 16:45:04 +0900

    chore: 배포 리전 위치 수정

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,12 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v2";
 import * as admin from "firebase-admin";
 import next from "next";
+import { setGlobalOptions } from "firebase-functions/v2";
+
+// 글로벌 리전 설정
+setGlobalOptions({
+  region: "asia-northeast1", // 한국 리전 설정
+});
 
 // Firebase Admin 초기화
 admin.initializeApp();
