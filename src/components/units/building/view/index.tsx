1f0893f7082af3c4e90cc3b49fbcbb1b3b42ec74
Calmer_com <ty.calmer96@gmail.com>
2025-02-21 12:05:05 +0900

    refactor: 맵 로직은 네이버맵 컴포넌트로 이동

--- src/components/units/building/view/index.tsx
+++ src/components/units/building/view/index.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import axios from "axios";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useFirestore } from "@/src/hooks/firebase/useFirestore";
-import { loadScript } from "@/src/commons/libraries/utils/maps/init";
-// import { useMapsLoader } from "@/src/hooks/maps/useMapsLoader";
 import { engToKor } from "@/src/commons/libraries/utils/convertCollection";
 
 import LoadingSpinner from "@/src/components/commons/loadingSpinner";
@@ -16,85 +14,6 @@ import * as S from "./styles";
 import "./marker.css";
 import { DEFAULT_STRING_VALUE } from "@/src/commons/constants";
 import type { IAssetForm, IBuildingItem, IBuildingParamsPromiseProps, IFirestore, IGeocode, IGeocodeData } from "@/src/commons/types";
-
-interface IMarkerIconContentParams {
-  geocodeData: IGeocodeData;
-  matchingData: IFirestore[];
-}
-interface ICreateMarkerParams {
-  geocodeData: IGeocodeData;
-  matchingData: IFirestore[];
-  setSelectedMarkerData: (data: IGeocodeData) => void;
-}
-interface IClusterIcon {
-  content: string;
-  size: any;
-  anchor: any;
-}
-
-const markerIconContent = ({ geocodeData, matchingData }: IMarkerIconContentParams): string => {
-  const isMatched = matchingData.some((matchingData) => matchingData.address === geocodeData.geocode.jibunAddress || matchingData.address === geocodeData.geocode.roadAddress);
-
-  const amount = (Number(geocodeData.data?.dealAmount?.replace(/,/g, "") ?? "0") / 10000).toFixed(2);
-  const peng = Math.round(geocodeData.data?.excluUseAr * 0.3025);
-
-  return `
-    <div class="markerBox ${isMatched ? "hasData" : ""}">
-      <div class="top">${peng}평</div>
-      <div class="bottom"> 
-      <span>매</span> <strong>${amount}억</strong></div>
-    </div>`;
-};
-
-const createMarker = ({ geocodeData, matchingData, setSelectedMarkerData }: ICreateMarkerParams): any => {
-  if (geocodeData === null) return;
-  const markerOptions = {
-    position: new window.naver.maps.LatLng(geocodeData.geocode?.latitude, geocodeData.geocode?.longitude),
-    map: null, // Set map to null initially
-    icon: {
-      content: markerIconContent({ geocodeData, matchingData }),
-    },
-  };
-
-  const marker = new window.naver.maps.Marker(markerOptions);
-  marker.set("data", geocodeData);
-
-  window.naver.maps.Event.addListener(marker, "click", () => {
-    if (geocodeData.data !== undefined) {
-      setSelectedMarkerData(geocodeData);
-    }
-  });
-
-  return marker;
-};
-
-const createClusterIcons = (): IClusterIcon[] => {
-  const icons = [];
-  for (let i = 1; i <= 5; i++) {
-    icons.push({
-      content: `<div class="cluster" style="background-image: url(https://navermaps.github.io/maps.js.ncp/docs/img/cluster-marker-${i}.png);"></div>`,
-      size: new window.naver.maps.Size(40, 40),
-      anchor: new window.naver.maps.Point(20, 20),
-    });
-  }
-  return icons;
-};
-
-const clusteringOptions = (map: any, markers: any[]): any => {
-  return new window.MarkerClustering({
-    minClusterSize: 5, // 클러스터 최소 마커 개수 증가
-    maxZoom: 16, // 최대 줌 레벨 최적화
-    map,
-    markers,
-    disableClickZoom: false,
-    gridSize: 300, // 그리드 크기 최적화
-    icons: createClusterIcons(),
-    indexGenerator: [10, 20, 50, 100, 200],
-    stylingFunction: (clusterMarker: any, count: any) => {
-      clusterMarker.getElement().querySelector("div:first-child").innerText = count;
-    },
-  });
-};
 
 export default function BuildingView({ params }: IBuildingParamsPromiseProps): JSX.Element {
   const [buildingType, setBuildingType] = useState<string | undefined>(undefined);
@@ -226,99 +145,6 @@ export default function BuildingView({ params }: IBuildingParamsPromiseProps): J
   }, [allGeocodeData, firestoreData, buildingType]);
 
   // 맵 마커 로직
-  const markersRef = useRef<any[]>([]);
-  const markerClusteringRef = useRef<any>();
-  const updateMarkers = useCallback(
-    async (map: any) => {
-      // console.log("Current geocodeData:", geocodeData); // 여기서 geocodeData 상태를 확인
-      const mapBounds = map.getBounds();
-      const processedPositions = new Set<string>();
-
-      markersRef.current.forEach((marker) => marker.setMap(null));
-      markersRef.current = [];
-
-      // 기존의 api에서 가져온 데이터, 새로 등록한 데이터
-      allGeocodeData?.forEach((geocodeData) => {
-        const position = new window.naver.maps.LatLng(geocodeData.geocode?.latitude, geocodeData.geocode?.longitude);
-        const positionKey = `${geocodeData.geocode?.latitude},${geocodeData.geocode?.longitude}`;
-
-        if (mapBounds.hasLatLng(position) === true && !processedPositions.has(positionKey)) {
-          const marker = createMarker({ geocodeData, matchingData, setSelectedMarkerData });
-          markersRef.current.push(marker);
-          processedPositions.add(positionKey); // 이미 처리한 위치는 Set에 추가
-        }
-      });
-      // 기존의 api에서 가져온 데이터, 새로 등록한 데이터
-      // registeredGeocodeDatas.newDatas?.forEach((newData) => {
-      //   const position = new window.naver.maps.LatLng(newData.geocode?.latitude, newData.geocode?.longitude);
-      //   const positionKey = `${newData.geocode?.latitude},${newData.geocode?.longitude}`;
-
-      //   if (mapBounds.hasLatLng(position) === true && !processedPositions.has(positionKey)) {
-      //     const marker = createMarkerUser({ newData: registeredGeocodeDatas.matchingData, setSelectedMarkerData });
-      //     markersRef.current.push(marker);
-      //     processedPositions.add(positionKey); // 이미 처리한 위치는 Set에 추가
-      //   }
-      // });
-
-      if (markerClusteringRef.current != null) {
-        markerClusteringRef.current.setMap(null);
-      }
-      markerClusteringRef.current = clusteringOptions(map, markersRef.current);
-
-      const markerDataArray: IGeocodeData[] = markersRef.current.map((marker) => marker.get("data"));
-
-      setVisibleMarkerData(markerDataArray);
-      setSelectedMarkerData(undefined);
-    },
-    [allGeocodeData, matchingData, setVisibleMarkerData, setSelectedMarkerData]
-  );
-
-  const isClusterScriptLoadedRef = useRef(false);
-  const loadClusterScript = useCallback(
-    (map: any) => {
-      if (isClusterScriptLoadedRef.current) {
-        // console.log("클러스터 스크립트가 이미 로드되었습니다.");
-
-        // 기존 리스너 제거 후 등록
-        window.naver.maps.Event.clearListeners(map, "idle");
-        window.naver.maps.Event.addListener(map, "idle", () => {
-          void updateMarkers(map);
-        });
-        void updateMarkers(map);
-        return;
-      }
-
-      const MARKER_CLUSTERING_SCRIPT_URL = "/libraries/markerClustering.js";
-      loadScript(MARKER_CLUSTERING_SCRIPT_URL, () => {
-        try {
-          // console.log("클러스터를 실행합니다.");
-          isClusterScriptLoadedRef.current = true; // 스크립트가 로드되었음을 기록
-
-          window.naver.maps.Event.addListener(map, "idle", () => {
-            void updateMarkers(map);
-          });
-          void updateMarkers(map);
-        } catch (error) {
-          console.error("클러스터 스크립트 로드 중 오류 발생:", error);
-        }
-      });
-    },
-    [updateMarkers]
-  );
-
-  const onMapLoaded = useCallback(
-    (map: any) => {
-      // 첫 번째 geocode를 중심 좌표로 설정
-      if (geocode !== undefined) {
-        const firstPosition = new window.naver.maps.LatLng(geocode.latitude, geocode.longitude);
-        map.setCenter(firstPosition); // 지도 중심 설정
-      }
-
-      loadClusterScript(map); // 클러스터 스크립트 로드
-    },
-    [geocode, loadClusterScript]
-  );
-  // const { mapLoading } = useMapsLoader({ onMapLoaded });
 
   // params를 비동기적으로 처리하려면 await로 기다려야 함
   useEffect(() => {
@@ -362,9 +188,12 @@ export default function BuildingView({ params }: IBuildingParamsPromiseProps): J
           mapMode={mapMode}
           setMapMode={setMapMode}
           setAsset={setAsset}
-          // mapLoading={mapLoading}
-          onMapLoaded={onMapLoaded}
+          geocode={geocode}
+          allGeocodeData={allGeocodeData}
           allGeocodeDataLoading={allGeocodeDataLoading}
+          matchingData={matchingData}
+          setSelectedMarkerData={setSelectedMarkerData}
+          setVisibleMarkerData={setVisibleMarkerData}
           setRegionName={setRegionName}
           setRegionCode={setRegionCode}
         />
