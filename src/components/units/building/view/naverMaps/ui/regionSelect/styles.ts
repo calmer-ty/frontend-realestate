18da53392bbbc99f3f8c14754076a910c6ea0adc
Calmer_com <ty.calmer96@gmail.com>
2025-02-26 09:11:12 +0900

    styles: RegionSelect 스타일 수정

--- src/components/units/building/view/naverMaps/ui/regionSelect/styles.ts
+++ src/components/units/building/view/naverMaps/ui/regionSelect/styles.ts
@@ -3,8 +3,8 @@ import { colors } from "@/src/commons/styles";
 
 export const Container = styled.div`
   display: flex;
-  outline: 0.125rem solid #1976d2;
-  border-radius: 1.875rem;
+  outline: 0.0625rem solid ${colors.outline};
+  border-radius: 0.25rem;
   background-color: ${colors.background};
   .MuiFormControl-root {
     justify-content: center;
