5fe49fc7416d2b0acd5c2b14c186a45dcf892a60
Calmer <ty.calmer96@gmail.com>
2025-04-20 15:24:49 +0900

    fix: 프로젝트 이름 변경

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import type { IMetadata } from "@/src/commons/types";
 
 export const generateMetadata = async (): Promise<IMetadata> => {
   return {
-    title: "부동산 시뮬레이터",
+    title: "부동산 거래 서비스",
     description: "거래된 매물을 조회하고, 직접 매물을 등록해보세요.",
     openGraph: {
-      title: "부동산 시뮬레이터",
+      title: "부동산 거래 서비스",
       description: "거래된 매물을 조회하고, 직접 매물을 등록해보세요.",
       images: ["https://www.calmer96.store/images/main.jpg"],
       url: "/images/main.jpg",
