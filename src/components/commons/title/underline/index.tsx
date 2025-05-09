bea638cfbe862ac32819b5c7df211a37d9f5c557
Calmer <ty.calmer96@gmail.com>
2025-05-10 02:15:35 +0900

    fix: 밑줄 제목 컴포넌트 수정

--- src/components/commons/title/underline/index.tsx
+++ src/components/commons/title/underline/index.tsx
@@ -1,10 +1,8 @@
-import { Box } from "@mui/material";
-
 export default function UnderlineTitle({ label, desc }: { label: string; desc?: string }): JSX.Element {
   return (
-    <Box sx={{ display: "flex", alignItems: "center", justifyContent: "space-between", borderBottom: "0.1875rem solid #000", paddingBottom: "0.625rem" }}>
-      <h2>{label}</h2>
+    <div className="flex items-center justify-between border-b-[0.1875rem] border-black pb-[0.625rem]">
+      <h2 className="font-bold text-2xl">{label}</h2>
       <p>{desc}</p>
-    </Box>
+    </div>
   );
 }
